4c556e02bbdfdd3bb6b097f158737a60a9041b70
RqfaelJr <ribeiro.rafajunior@gmail.com>
2025-11-21 16:12:29 -0300

    Refactor: refatora nome de tela de admin

--- eventos/admin.py
+++ eventos/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-
 from eventos.models import Categoria
 
-# --- Configuração para Notificações ---
 @admin.register(Categoria)
-class NotificacaoAdmin(admin.ModelAdmin):
+class CategoriaAdmin(admin.ModelAdmin):
     list_display = ('nome', )
     
     list_filter = ('nome',)
